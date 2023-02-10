8e6542c946d6625e357cad111a9789e8c17165b1
EpicShadowXD <siminischi1234@gmail.com>
2023-02-10 19:24:54 +0200

    Started working on parser syntax checker

--- SimiParser.cpp
+++ SimiParser.cpp
@@ -17,6 +17,15 @@ private:
     std::vector<std::string> keyVector;
     std::map<std::string, size_t> variableMap;
 
+    std::vector<std::string> registerVector = {
+            "EA",
+            "EB",
+            "EAX",
+            "EBX",
+            "ZX",
+            "ZY",
+    };
+
 public:
     SimiParser(const std::string& newFileName) : fileName(newFileName) {
         // I hate having to do this because file standards differ so much from os to os
@@ -46,11 +55,16 @@ public:
     }
 
     // Has no syntax checking, basically just be good
+    // TODO: currently working on syntax checker and error handler
+    // TODO: clean up comprehenser and modularise it.
     void comprehenser() {
         for (size_t pos = 0; pos < keyVector.size(); pos++) {
             if (keyVector[pos] == "INT")
             {
                 pos++;
+
+                if (!isLValue(keyVector[pos]))
+                    throw "Expected register (lvalue)";
 
                 std::string variableName = keyVector[pos];
                 size_t variableValue = std::stoi(keyVector[pos + 1]);
@@ -96,6 +110,14 @@ public:
                 std::cout << variableMap[keyVector[pos]];
             }
         }
+    }
+
+    bool isLValue(std::string command) {
+        if (std::find(registerVector.begin(),
+                      registerVector.end(),
+                      command) != registerVector.end())
+            return true;
+        return false;
     }
 
 };
